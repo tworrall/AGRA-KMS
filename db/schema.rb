cf2007a23f80b4bcccd2769dce1e2dad3567e406
tlw72 <tlw72@aws-110-048.internal.library.cornell.edu>
2018-05-21 20:57:33 -0400

    user management changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180520201926) do
+ActiveRecord::Schema.define(version: 20180522003357) do
 
   create_table "bookmarks", id: :integer, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer "user_id", null: false
@@ -565,6 +565,7 @@ ActiveRecord::Schema.define(version: 20180520201926) do
     t.binary "zotero_token"
     t.string "zotero_userid"
     t.string "preferred_locale"
+    t.boolean "admin"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
