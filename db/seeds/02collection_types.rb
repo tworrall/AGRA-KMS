273e6ef3c6139aff5cdd636bc069f33fa912a0dc
John Fereira <jaf30@cornell.edu>
2018-05-29 17:08:34 +0000

    initial version of db seed files

---
+++ db/seeds/02collection_types.rb
@@ -0,0 +1,53 @@
+case Rails.env
+when 'development', 'integration', 'staging', 'production'
+  puts 'Adding collection types...'
+
+  Hyrax::CollectionType.find_or_create_admin_set_type
+  as_ct = Hyrax::CollectionType.find_by(machine_id: Hyrax::CollectionType::ADMIN_SET_MACHINE_ID)
+  as_ct.title = 'Administrative Set'
+  as_ct.badge_color = '#405060'
+  as_ct.save
+
+  user_ct = Hyrax::CollectionType.find_by(machine_id: Hyrax::CollectionType::USER_COLLECTION_MACHINE_ID)
+  unless user_ct.present?
+    options = {
+      description: Hyrax::CollectionTypes::CreateService::USER_COLLECTION_OPTIONS[:description],
+      nestable: true,
+      brandable: false,
+      discoverable: false,
+      sharable: true,
+      share_applies_to_new_works: false,
+      allow_multiple_membership: true,
+      require_membership: false,
+      assigns_workflow: false,
+      assigns_visibility: false,
+      badge_color: '#705070',
+      participants: [{ agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: 'admin', access: Hyrax::CollectionTypeParticipant::MANAGE_ACCESS },
+                     { agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: ::Ability.registered_group_name, access: Hyrax::CollectionTypeParticipant::CREATE_ACCESS }]
+    }
+    Hyrax::CollectionTypes::CreateService.create_collection_type(machine_id: Hyrax::CollectionType::USER_COLLECTION_MACHINE_ID, title: 'User Collection', options: options)
+    puts 'Created User Collection Type'
+  end
+
+  org_ct = Hyrax::CollectionType.find_by(machine_id: 'organization')
+  unless org_ct.present?
+    options = {
+      description: 'Collection units that can be nested',
+      nestable: true,
+      brandable: true,
+      discoverable: true,
+      sharable: true,
+      share_applies_to_new_works: true,
+      allow_multiple_membership: false,
+      require_membership: false,
+      assigns_workflow: false,
+      assigns_visibility: false,
+      badge_color: '#663333',
+      participants: [{ agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: 'admin', access: Hyrax::CollectionTypeParticipant::MANAGE_ACCESS },
+                     { agent_type: Hyrax::CollectionTypeParticipant::GROUP_TYPE, agent_id: 'admin', access: Hyrax::CollectionTypeParticipant::CREATE_ACCESS }]
+    }
+    Hyrax::CollectionTypes::CreateService.create_collection_type(machine_id: 'organization', title: 'AGRA Collection', options: options)
+    puts 'Created Organization Collection Type'
+  end
+  puts 'Done.'
+end
---
+++ db/seeds/03collections.rb
@@ -0,0 +1,101 @@
+#
+# Create Collections
+#
+
+# Create a public  collection
+# @param [User] user creating the collection
+# @param [String] collection type gid
+# @param [String] id to use for the new collection
+# @param [Hash] options holding metadata and permissions for the new collection
+def create_collection(user, type_gid, id, options)
+  # find existing collection if it already exists
+  col = Collection.where(id: id)
+  return col.first if col.present?
+
+  # remove stale permisisons for the collection id
+  remove_access(collection_id: id)
+
+  # create collection
+  col = Collection.new(id: id)
+  col.attributes = options.except(:permissions)
+  col.apply_depositor_metadata(user.user_key)
+  col.collection_type_gid = type_gid
+  col.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC
+  col.save
+
+  # apply new permissions
+  add_access(collection: col, grants: options[:permissions])
+  col
+end
+
+# Add access grants to a collection
+# @param collection [Collection] the collection to modify
+# @param grants [Array<Hash>] array of grants to add to the collection
+# @example grants
+#   [ { agent_type: Hyrax::PermissionTemplateAccess::GROUP,
+#       agent_id: 'my_group_name',
+#       access: Hyrax::PermissionTemplateAccess::DEPOSIT } ]
+# @see Hyrax::PermissionTemplateAccess for valid values for agent_type and access
+def add_access(collection:, grants:)
+  template = Hyrax::PermissionTemplate.find_or_create_by(source_id: collection.id)
+  grants.each do |grant|
+    Hyrax::PermissionTemplateAccess.find_or_create_by(permission_template_id: template.id,
+                                                      agent_type: grant[:agent_type],
+                                                      agent_id: grant[:agent_id],
+                                                      access: grant[:access])
+  end
+  collection.reset_access_controls! # updates solr
+end
+
+# Remove all access grants for a specific collection id
+# @param collection_id [String] the id of stale collection
+def remove_access(collection_id:)
+  templates = Hyrax::PermissionTemplate.where(source_id: collection_id)
+  return unless templates.present?
+  templates.each { |template| template.destroy! }
+end
+
+case Rails.env
+when 'development', 'integration', 'staging', 'production'
+
+  organization_gid = CollectionTypeService.organization_gid
+
+  unless organization_gid.present?
+    puts 'Failed to get Agra collection type.  Unable to create collections.'
+    return
+  end
+
+  user = User.find_by(email: ENV['ADMIN_EMAIL'])
+  unless user.present?
+    puts 'Failed to get admin user.  Unable to create collections.'
+    return
+  end
+
+  base_grants = [ { agent_type: Hyrax::PermissionTemplateAccess::GROUP, agent_id: 'admin', access: Hyrax::PermissionTemplateAccess::MANAGE } ]
+
+  puts 'Adding Collections...'
+
+  unless Collection.exists?(title: 'Training')
+    permissions = base_grants.dup
+    permissions << { agent_type: Hyrax::PermissionTemplateAccess::GROUP, agent_id: 'admin', access: Hyrax::PermissionTemplateAccess::MANAGE }
+    permissions << { agent_type: Hyrax::PermissionTemplateAccess::GROUP, agent_id: 'admin', access: Hyrax::PermissionTemplateAccess::DEPOSIT }
+    training = create_collection(user, organization_gid, 'training',
+                            title: ['Training'],
+                            description: ['Training Collection'],
+                            permissions: permissions)
+
+    puts 'Created Training Collection'
+  end
+
+  unless Collection.exists?(title: 'Agra KMS')
+    permissions = base_grants.dup
+    permissions << { agent_type: Hyrax::PermissionTemplateAccess::GROUP, agent_id: 'admin', access: Hyrax::PermissionTemplateAccess::MANAGE }
+    permissions << { agent_type: Hyrax::PermissionTemplateAccess::GROUP, agent_id: 'admin', access: Hyrax::PermissionTemplateAccess::DEPOSIT }
+    agrakms = create_collection(user, organization_gid, 'agra_kms',
+                            title: ['Agra KMS'],
+                            description: ['Agra KMS Collection'],
+                            permissions: permissions)
+    puts 'Created Agra KMS Collection'
+  end
+  puts 'Done.'
+end
