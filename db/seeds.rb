dc154e554e3896e3405cac9aaa087accb872420d
John Fereira <jaf30@cornell.edu>
2018-05-29 17:07:19 +0000

    add fedora and solr clean

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# if ENV["RAILS_SKIP_SEEDS"] == "false"
+  case Rails.env
+    when "development", "integration"
+    puts "Cleaning out Fedora and Solr"
+    require 'active_fedora/cleaner'
+    ActiveFedora::Cleaner.clean!
+    Hyrax::PermissionTemplateAccess.destroy_all
+    Hyrax::PermissionTemplate.destroy_all
+  end
+  Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each { |seed| load seed }
+#end
