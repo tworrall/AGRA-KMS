9a43c59dcac27e91acf4c0563b4959a1465411a0
John Fereira <jaf30@cornell.edu>
2018-05-30 16:39:46 +0000

    add Test collection type

--- app/services/collection_type_service.rb
+++ app/services/collection_type_service.rb
@@ -1,10 +1,16 @@
 class CollectionTypeService
   ORGANIZATION_MACHINE_ID = 'organization'.freeze
+  TEST_MACHINE_ID = 'test'.freeze
   USER_COLLECTION_MACHINE_ID = Hyrax::CollectionType::USER_COLLECTION_MACHINE_ID
 
   # Return true if the collection has type Organization
   def self.organization?(collection)
     collection.collection_type_gid == organization_gid
+  end
+  
+  # Return true if the collection has type Test
+  def self.test?(collection)
+    collection.collection_type_gid == test_gid
   end
   
   # Return true if the collection has type User Collection
@@ -15,6 +21,11 @@ class CollectionTypeService
   # Return the Organization collection type's gid
   def self.organization_gid
     gid(ORGANIZATION_MACHINE_ID)
+  end
+  
+  # Return the Test collection type's gid
+  def self.test_gid
+    gid(TEST_MACHINE_ID)
   end
   
   # Return the User Collection collection type's gid
